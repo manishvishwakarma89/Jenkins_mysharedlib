69013c8dee612999364277a079705fc461686b96
manishvishwakarma89 <manish.kumar.v@ramanujan.du.ac.in>
2026-06-11 00:15:34 +0530

    Modify Trivy scan function to accept parameters
    Enhanced the Trivy scan function to accept parameters for exit code and severity levels, and added output handling.

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,16 @@
-def call(){
-  sh "trivy fs ."
+def call(String exitCode = "0", String severity = "HIGH,CRITICAL"){
+    sh """
+        trivy fs \
+          --format table \
+          --severity ${severity} \
+          --exit-code ${exitCode} \
+          --no-progress \
+          -o trivy-fs-report.txt \
+          .
+        
+        echo "Trivy scan completed"
+        cat trivy-fs-report.txt
+    """
+    
+    archiveArtifacts artifacts: 'trivy-fs-report.txt', allowEmptyArchive: true
 }
