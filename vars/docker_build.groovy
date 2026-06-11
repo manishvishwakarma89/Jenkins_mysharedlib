143c99334368198638d6898b7e8fdd1828a2f8a4
manishvishwakarma89 <manish.kumar.v@ramanujan.du.ac.in>
2026-06-11 17:49:42 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,6 +1,5 @@
 def call(String imageName, String tag, String dockerHubUser){
     sh """
-        docker build -t ${dockerHubUser}/${imageName}-backend:${tag} ./backend
-        docker build -t ${dockerHubUser}/${imageName}-frontend:${tag} ./frontend
+        docker build -t ${dockerHubUser}/${imageName}:${tag} .
     """
 }
